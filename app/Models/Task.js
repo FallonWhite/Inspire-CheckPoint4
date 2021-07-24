cb4f96022f131f698a4619f754930811385de0e5
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-24 15:14:56 -0600

    TaskModelService

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -1,14 +1,35 @@
+import { ProxyState } from "../AppState.js"
+
 export default class Task {
-    constructor(data) {
-        this.title = data.title
+    constructor({ name, id, checked }) {
+        this.id = id
+        this.name = name
+        this.total = 1
+        this.checked = checked
     }
 
     get Template() {
+        console.log(this.checked)
+        return `
+    <div class="${this.id}">
+    <input type="checkbox" style="background-color: ${this.name}" class="mr-2" onclick="app.tasksController.checkBox('${this.id}')" ${this.checked ? 'checked' : ''}>
+    ${this.name} 
+<button type="destroy" class="btn btn-primary btn-outline-info btn-sm my-1 ml-5 pull-right" onclick="app.tasksController.removeTask('${this.id}')">Delete</button>`
+    }
 
-        return /*html*/`
-        <div class="card p-2 task">
-            ${this.title}
-        </div>
-        `
+    //to display task total//
+    get MyTasks() {
+        let template = ''
+        let tasksTotal = 0
+        let tasks = ProxyState.tasks.filter(task => task.id === this.id)
+        tasks.forEach(t => {
+            template += t.Template
+            tasksTotal += t.total
+        })
+
+        if (!template) {
+            template += "No tasks"
+        }
+        return [template, tasksTotal]
     }
 }
--- app/Services/TasksService.js
+++ app/Services/TasksService.js
@@ -3,7 +3,7 @@ import Task from "../Models/Task.js";
 
 class TasksService {
   addTask() {
-    ProxyState.tasks = [...ProxyState.tasks, new Task("")]
+    ProxyState.tasks = [...ProxyState.tasks, new Task()]
   }
   removeTask(id) {
     ProxyState.tasks = ProxyState.tasks.filter(task => task.id != id)
