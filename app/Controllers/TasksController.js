6a704a9acd61152a8af715a18f7282a78790a294
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-24 15:26:54 -0600

    break

--- app/Controllers/TasksController.js
+++ app/Controllers/TasksController.js
@@ -4,26 +4,30 @@ import { tasksService } from "../Services/TasksService.js";
 
 //Private
 function _draw() {
-  let tasks = ProxyState.tasks;
   let template = ''
-  tasks.forEach(t => template += t.Template)
-  document.getElementById("app").innerHTML = /*html*/`
-  <button className="btn btn-info" onclick="app.tasksController.addTask()">Add Task</button>  
-  <div className="card-columns values">
-      ${template}
-  </div>
-  `
+  let tasks = ProxyState.tasks;
+  tasks.forEach(tasks => template += tasks.Template)
+  document.getElementById('tasks').innerHTML = template
 }
 
 //Public
 export default class TasksController {
   constructor() {
     ProxyState.on("tasks", _draw);
+
     _draw()
   }
 
-  addTask() {
+  addTask(id) {
+    event.preventDefault()
+    let form = event.target
+    let newTask = {
+      id,
+      // @ts-ignore
+      name: form.task.value,
+    }
     tasksService.addTask()
+    // @ts-ignore
+    form.reset()
   }
-
 }
