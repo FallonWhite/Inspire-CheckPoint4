a932eb93d6580030aa27424f064ba7e052663609
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-25 15:41:18 -0600

    Break-Nothing is working

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -7,14 +7,10 @@ export default class Weather {
   get Template() {
 
     return `
-    <div class="row d-flex justify-content-end">
-    <div class="col-md-1 d-flex justify-content-end flex-column align-items-end mt-3 mr-3" id="weather">
         <i class="fa fa-cloud-moon-rain d-flex "></i>
         <h5 class="m-0">${this.main}</h5>
         </fa>
         <h5>${this.weather}</h5>
-    </div>
-</div>
 `
   }
 
