18d84ca1801d85fed7c7acd51487932dcecc4591
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-25 13:13:38 -0600

    image

---
+++ app/Controllers/ImagesController.js
@@ -0,0 +1,22 @@
+import { ProxyState } from "../AppState.js";
+import { imagesService } from "../Services/ImagesService.js";
+
+function _draw() {
+    document.getElementById("image").style.backgroundImage = `url(${ProxyState.images.image})`
+    console.log('draw ' + ProxyState.image)
+}
+
+export default class ImagesController {
+    constructor() {
+        ProxyState.on('images', _draw);
+        this.getImage()
+    }
+
+    async getImage() {
+        try {
+            await imagesService.getImage()
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
---
+++ app/Services/ImagesService.js
@@ -0,0 +1,15 @@
+import { ProxyState } from "../AppState.js"
+import Image from "../Models/Image.js"
+import { imagesApi } from './AxiosService.js'
+
+class ImagesService {
+  constructor() {
+    this.getImage()
+  }
+  async getImage() {
+    let res = await imagesApi.get('')
+    ProxyState.images = new Image(res.data);
+  }
+}
+
+export const imagesService = new ImagesService()
