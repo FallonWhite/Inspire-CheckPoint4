75017053d6d54005a1f79a711a0c44dc37678026
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-24 18:45:39 -0600

    break

---
+++ app/Services/AxiosService.js
@@ -0,0 +1,26 @@
+import Task from "../Models/Task"
+
+export const inspireApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api',
+  timeout: 10000
+})
+
+export const weatherApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/weather',
+  timeout: 10000
+})
+
+export const quotesApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/quotes',
+  timeout: 10000
+})
+
+export const imagesApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/images',
+  timeout: 10000
+})
+
+export const tasksApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/fallon/todo',
+  timeout: 10000
+})
--- app/Services/TasksService.js
+++ app/Services/TasksService.js
@@ -1,7 +1,13 @@
 import { ProxyState } from "../AppState.js";
 import Task from "../Models/Task.js";
+import { tasksApi } from "./AxiosService.js";
 
 class TasksService {
+  async setTasks(name) {
+    const res = await tasksApi.push('task')
+    ProxyState.tasks = new Task(res.data)
+    console.log(ProxyState.tasks)
+  }
   addTask() {
     ProxyState.tasks = [...ProxyState.tasks, new Task()]
   }
@@ -11,9 +17,12 @@ class TasksService {
   destroy(id) {
     ProxyState.tasks = ProxyState.tasks.filter(task => task.id != id)
   }
+
+  async postToSandbox() {
+    const res = await tasksApi.post('tasks', ProxyState.tasks)
+    console.log(new Task(res.data))
+  }
 }
-
 export const tasksService = new TasksService()
-
 
 
