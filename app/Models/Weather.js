3ced928f23404b4ce136cbc359de781277db873b
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-26 11:34:47 -0600

    break

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -14,9 +14,9 @@ export default class Weather {
     <div class="col-2 m-2" id="weather">
         <h4 class="m-1"><b>Weather: ${this.name}, ${this.description}, ${this.temp}°K</b></h4>
         <h5>${this.f}℉ ${this.c}℃</h5> 
-        <div class="d-flex justify-content-between">
-        <button class="btn btn-black btn-outline-primary" onclick="app.weatherController.getWeather('${this.c}')"> ℃</button>
-        <button class="btn btn-black btn-outline-primary" onclick="app.weatherController.getWeather('${this.f}')"> ℉</button>
+        <div class="d-flex justify-content-between m-2">
+        <button class="btn btn-black btn-outline-dark m-1" onclick="app.weatherController.getWeather('${this.c}')"> ℃</button>
+        <button class="btn btn-black btn-outline-dark m-1" onclick="app.weatherController.getWeather('${this.f}')"> ℉</button>
         </div>
     </div>
 `
